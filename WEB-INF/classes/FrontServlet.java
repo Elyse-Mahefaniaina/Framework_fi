d3707471f7d973c3fc25c17c5e6628889314fa05
Elyse-Mahefaniaina <119030086+Elyse-Mahefaniaina@users.noreply.github.com>
2023-03-09 21:41:07 +0300

    commit sprint1, 2

---
+++ WEB-INF/classes/FrontServlet.java
@@ -0,0 +1,28 @@
+package etu1784.framework.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import util.Util;
+
+public class FrontServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+
+        PrintWriter out = response.getWriter();
+        out.print("url: " + new Util().processUrl(url, request.getContextPath()));
+    }
+}
